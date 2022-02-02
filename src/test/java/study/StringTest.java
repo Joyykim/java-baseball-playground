34f3d213e5a7c2b7f99968ac4c27ddba34db350a
joy <kjw11077@naver.com>
2022-02-03 00:01:31 +0900

    학습테스트 실습: 요구사항1 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,19 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split1() {
+        String[] splited = "1,2".split(",");
+        assertThat(splited).hasSize(2);
+        assertThat(splited[0]).isEqualTo("1");
+        assertThat(splited[1]).isEqualTo("2");
+    }
+
+    @Test
+    void split2() {
+        // "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
+        String[] splited = "1".split(",");
+        assertThat(splited).containsExactly("1");
+    }
 }
